cb2b5973bfd92fe544b8b71b56b2abc0d35d1061
Mathenge-Alex <arxmaina@gmail.com>
2022-04-24 14:01:30 +0300

    refact: rename class CAUser

--- User.py
+++ User.py
@@ -1,15 +1,15 @@
 from typing import Dict, Any
 
 
-class User:
-    user_accounts: Dict[Any, Any] = {}
+class CAUser:
+    user_account = []
 
     def __int__(self, username, password):
         self.username = username
         self.password = password
 
-    def create_account(self):
-        User.user_accounts.update(self)
+    def save_account(self):
+        CAUser.user_account.append(self)
 
     def delete_account(self):
-        User.user_accounts.pop(self)
+        CAUser.user_account.remove(self)
